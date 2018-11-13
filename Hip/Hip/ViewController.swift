96e1ae1f50824703210b73672d9dd39f8bd0dbf7
Kids Macbook <kidsmacbook@Kidss-MacBook-Pro.local>
2018-11-13 11:01:40 -0500

    trying to get current location

--- Hip/Hip/ViewController.swift
+++ Hip/Hip/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 import GoogleMaps
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,GMSMapViewDelegate,CLLocationManagerDelegate{
+    @IBOutlet weak var mapView: GMSMapView!
+var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        mapView.isMyLocationEnabled=true
+        mapView.delegate=self
+        
+        self.locationManager.delegate = self
+        self.locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    //Location Manager delegates
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude : (location?.coordinate.longitude)!,zoom:14)
+        mapView.animate(to: camera)
+        
+        //Finally stop updating loaction otherwise it will come again and again in this delegate
+        self.locationManager.stopUpdatingLocation()
+        
+    }
+    
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+       
         view = mapView
         
         // Creates a marker in the center of the map.
